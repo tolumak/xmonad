e731570788687af8d134916490a9411f1868f268
Michel Lafon-Puyo <michel.lafonpuyo@gmail.com>
2011-04-19 17:35:50 +0200

    vmware-arch host: use thunderbird and DPMS settings

--- autostart-vmware-arch.sh
+++ autostart-vmware-arch.sh
@@ -3,6 +3,12 @@
 # Cursor
 xsetroot -cursor_name left_ptr
 
+# Screensaver off
+xset s off
+
+# DPMS delays
+xset dpms 0 300 1200
+
 # Trayer
 /usr/bin/trayer --edge top --align right --widthtype percent --width 10 --height 17 --tint 0 --transparent true --alpha 1 --SetDockType true &
 
@@ -13,4 +19,4 @@ firefox &
 emacs &
 ~/bin/mail_tunnel.sh
 pidgin &
-kmail &
+thunderbird &
