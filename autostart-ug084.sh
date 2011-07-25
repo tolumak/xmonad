0340c2c373621f01f762a49f21a671f69e8f3705
Michel Lafon-Puyo <mlafon-puyo@sise.fr>
2011-07-25 12:18:59 +0200

    stalonetray is running in the background (+ permission fixes)

--- autostart-ug084.sh
+++ autostart-ug084.sh
@@ -10,7 +10,7 @@ sh ~/.fehbg &
 xsetroot -cursor_name left_ptr
 
 # Trayer
-/usr/bin/stalonetray -bg \#000000 --icon-gravity SE -i 15 -s 16 --geometry 12x1+1088+0 --max-geometry 12x1
+/usr/bin/stalonetray -bg \#000000 --icon-gravity SE -i 15 -s 16 --geometry 12x1+1088+0 --max-geometry 12x1 &
 
 # Launch applications
 xbindkeys &
