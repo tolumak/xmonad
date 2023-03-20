fcc28f3aef5a3743477463eb583c99e540d5204c
Michel LAFON-PUYO <michel.lafonpuyo@gmail.com>
2023-03-20 12:25:35 +0100

    add configuration for latitude laptop

---
+++ autostart-latitude.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Background
+#sh ~/.fehbg &
+
+# ICC
+
+# Cursor
+
+# Trayer
+/usr/bin/stalonetray --icon-gravity SE -i 15 -s 16 --geometry 12x1+1728+0 --max-geometry 12x1 &
+
+# Launch applications
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+xbindkeys &
+nm-applet &
+pasystray &
+firefox &
+thunderbird &
+
